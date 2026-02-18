63c7406116cbfa92b27ba7322ca523dfab69235f
KOROBYAKA <wads31566@gmail.com>
2026-02-18 15:29:45 +0200

    UdpTeeSock basic implementation added

--- src/quic_tap.rs
+++ src/quic_tap.rs
@@ -2,7 +2,40 @@
 struct TeeUdpSock {
     udp_sock: Arc<dyn AsyncUdpSocket>,
     send_channel: crossbeam_channel::unbounded::sender,
-    sock_id: u32,
+    local_addr: SocketAddr,
+}
+
+impl TeeUdpSock {
+    fn new(real_udp_sock: Arc<dyn AsyncUdpSocket>, channel: Sender) -> TeeUdpSock {
+        TeeUdpSock {
+            udp_sock: real_udp_sock,
+            send_channel: channel,
+            local_addr: real_udp_sock.local_addr(),
+        }
+    }
+}
+
+impl AsyncUdpSocket for TeeUdpSock {
+    fn create_io_poller(self: Arc<Self>) -> Pin<Box<dyn UdpPoller>> {
+        self.udp_sock.create_io_poller()
+    }
+
+    fn poll_recv(
+        &self,
+        cx: &mut Context<'_>,
+        bufs: &mut [IoSliceMut<'_>],
+        meta: &mut [RecvMeta],
+    ) -> Poll<Result<usize>> {
+        self.udp_sock.poll_recv(cx, bufs, meta)
+    }
+
+    fn local_addr(&self) -> Result<SocketAddr> {
+        Ok(self.local_addr)
+    }
+
+    fn try_send(&self, data_to_transmit: &Transmit<'_>) -> Result<()> {
+        self.udp_sock.try_send(data_to_transmit)
+    }
 }
 
 pub fn key_saver() {}
